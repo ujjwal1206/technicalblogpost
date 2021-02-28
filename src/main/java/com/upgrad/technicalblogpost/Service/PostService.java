b874c25ccb7d964379b079a19d5ef621a25e469a
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-02-28 15:26:38 +0530

    28feb2021

--- src/main/java/com/upgrad/technicalblogpost/Service/PostService.java
+++ src/main/java/com/upgrad/technicalblogpost/Service/PostService.java
@@ -7,26 +7,27 @@ import java.util.Date;
 @Service
 public class PostService {
     private static ArrayList<Post> POSTS=new ArrayList<Post>();
-    static {
-        Post post1= new Post();
-        post1.setTitle("Mirzapur");
-        post1.setBody("Kalin Bhaia ki sarkar");
-        post1.setDate(new Date());
-
-        Post post2= new Post();
-        post2.setTitle("Gangs of Wassepur");
-        post2.setBody("Keh ke lenege");
-        post2.setDate(new Date());
-
-        Post post3= new Post();
-        post3.setTitle("Kissan");
-        post3.setBody("Modi ke sarkar");
-        post3.setDate(new Date());
-
-        POSTS.add(post1);
-        POSTS.add(post2);
-        POSTS.add(post3);
-    }
+//    static {
+//        Post post1= new Post();
+//        post1.setTitle("Mirzapur");
+//        post1.setBody("Kalin Bhaia ki sarkar");
+//        post1.setDate(new Date());
+//
+//        Post post2= new Post();
+//        post2.setTitle("Gangs of Wassepur");
+//        post2.setBody("Keh ke lenege");
+//        post2.setDate(new Date());
+//
+//        Post post3= new Post();
+//        post3.setTitle("Kissan");
+//        post3.setBody("Modi ke sarkar");
+//        post3.setDate(new Date());
+//
+//        POSTS.add(post1);
+//        POSTS.add(post2);
+//        POSTS.add(post3);
+//    }
+    private final String url="";
 
     public ArrayList<Post> getAllPosts(){
         return POSTS;
